eee2043fd4ac9f23c0b0dd27e053aac7cc9ac2c1
Ana-BSL <anabeatrizna43@gmail.com>
2023-08-23 18:35:19 -0300

    primeiro commit

---
+++ modules/retalhino/controllers/ClienteController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\modules\retalhino\controllers;
+
+class ClienteController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
